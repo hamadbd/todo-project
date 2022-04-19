12a7f931e191d9cf6fa7414d0cd9ffc0126d8113
hamadbd <hamaddwairi0@gmail.com>
2022-04-20 02:16:30 +0300

    JS updated

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@ var gender = prompt('Please insert your gender as (male OR female) ...');
 var age = prompt('please insert your age ... ');
 if (age <=0)
 alert('Wrong number, please insert age again ...');
-var welcome = confirm('Do you want to skip the welcome message? ...');
-if (welcome=='no'||welcome=="No")
+if (!( confirm('Do you want to skip the welcome message? ...')))
 {
 if (gender=='male')
 alert('Welcome Mr '+ name+ ' to our website');
